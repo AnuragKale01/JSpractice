b3bfb340fc0625b6035f716e4aeaa1c380c3f78a
AnuragKale01 <130026913+AnuragKale01@users.noreply.github.com>
2024-12-13 05:59:24 +0000

    all container configuration

---
+++ 1st.js
@@ -0,0 +1,17 @@
+//data type data convertion and loop and conditional statement
+var name="anurag";
+var i=10;
+var j;
+var result;
+console.log(i);
+console.log(typeof(i));
+
+console.log(name);
+console.log(typeof(name));
+
+
+//Statement and loop//
+for(j=0;j<=name.length;j++){
+    console.log(j);
+}
+
